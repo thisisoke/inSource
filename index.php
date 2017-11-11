b73cb3f85bc171a9e1d2c58638aacad3ea7922a1
Seul Lee <27seul.lee@gmail.com>
2017-11-10 22:25:40 -0500

    add progress bar

--- index.php
+++ index.php
@@ -7,15 +7,39 @@
 </head>
 
 <body>
-  <nav>
     <nav>
       <li>Dashboard</li>
       <li>My Account</li>
       <li>Log out</li>
     </nav>
-  </nav>
+
+    <div class="progress">
+      <h1>Q3 Progress Bar</h1>
+      <p>Issue/suggestion submission opened. <b>Deadline: November 20th, 2017 11:59PM (EST)</b></p>
+      <a href="#" class="btn">Submit</a>
+      <div class="prg-bar-bg">
+        <div id="prg-bar-percent">
+        </div>
+      </div>
+      <span class="indicator-grp">
+        <div class="indicator-line"></div>
+        <a href="#">Submit</a>
+      </span>
+      <span class="indicator-grp">
+        <div class="indicator-line"></div>
+        <a href="#">Vote</a>
+      </span>
+      <span class="indicator-grp">
+        <div class="indicator-line"></div>
+        <a href="#">Meeting</a>
+      </span>
+      <span class="indicator-grp">
+        <div class="indicator-line"></div>
+        <a href="#">Summary</a>
+      </span>
+    </div>
      <?php
-      echo file_get_contents("submissionText.html");
+      echo file_get_contents("submissionText.html");//dummyf ile
     ?>
 </body>
 
